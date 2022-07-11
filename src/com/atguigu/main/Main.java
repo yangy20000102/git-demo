fcbcb7f22885d133213e74813c3c3fccbfbbac0b
yangy <2487755249@qq.com>
2022-07-11 21:49:39 +0800

    第三次提交

--- src/com/atguigu/main/Main.java
+++ src/com/atguigu/main/Main.java
@@ -7,7 +7,8 @@ package com.atguigu.main;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("main");
+        System.out.println("main1");
         System.out.println("main2");
+        System.out.println("main3");
     }
 }
