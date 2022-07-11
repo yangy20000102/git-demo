162e5b630b5f9d1140f57098a21e2bd482c3aa3a
yangy <2487755249@qq.com>
2022-07-11 21:43:50 +0800

    第一次提交

---
+++ src/com/atguigu/main/Main.java
@@ -0,0 +1,12 @@
+package com.atguigu.main;
+
+/**
+ * @author yangy
+ * @date 2022/7/11 21:37
+ * @Description
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("main");
+    }
+}
---
+++ src/com/atguigu/main/User.java
@@ -0,0 +1,15 @@
+package com.atguigu.main;
+
+/**
+ * @author yangy
+ * @date 2022/7/11 21:40
+ * @Description
+ */
+public class User {
+
+    private int id ;
+    private int age;
+    private String name;
+    private int sex ;
+
+}
